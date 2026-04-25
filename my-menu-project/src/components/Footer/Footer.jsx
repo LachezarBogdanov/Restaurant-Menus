b83d0aedfa0b11aeafdd1ee47c4518c36a34369e
LachezarBogdanov <lachezarbogdanov22@gmail.com>
2026-04-25 19:23:54 +0300

    Added footer

---
+++ my-menu-project/src/components/Footer/Footer.jsx
@@ -0,0 +1,23 @@
+import styles from './Footer.module.css'
+
+export default function Footer() {
+  return (
+    <>
+        <div className={styles.footer}>
+				<section className={styles.footerP}>
+					<p className={styles.footerInfo}>
+						Rezervations
+					</p>
+					<p className={styles.footerInfo}>
+						Contacts
+					</p>
+				</section>
+				<section className={styles.copyP}>
+					<p className={styles.copy}>
+						&copy; 2026 Lachezar Bogdanov
+					</p>
+				</section>
+			</div>
+    </>
+  );
+};
--- my-menu-project/src/components/Home/Home.jsx
+++ my-menu-project/src/components/Home/Home.jsx
@@ -1,13 +1,17 @@
+import Footer from '../Footer/Footer';
 import styles from './Home.module.css'
 
 export default function Home() {
-  return (
-    <>
-        <div className="head">
-            <img src='/home.jpg' alt="asd" className={styles.homeImg} />
-            <div className={styles.overlay}></div>
-        </div>
-        <div className={styles.cards}></div>
-    </>
-  );
+	return (
+		<>
+			<div className="head">
+				<img src='/home.jpg' alt="asd" className={styles.homeImg} />
+				<div className={styles.overlay}></div>
+			</div>
+
+			<div className={styles.cards}></div>
+
+			<Footer />
+		</>
+	);
 };
