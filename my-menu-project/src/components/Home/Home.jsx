def03d51f510b1c18b669fd6d4496307a9597a41
LachezarBogdanov <lachezarbogdanov22@gmail.com>
2026-04-26 19:56:43 +0300

    Added dynamic img and added h1 for the home page restaurant name

--- my-menu-project/src/components/Home/Home.jsx
+++ my-menu-project/src/components/Home/Home.jsx
@@ -9,14 +9,15 @@ export default function Home() {
 		<>
 			<div className="head">
 				<img src='/home.jpg' alt="asd" className={styles.homeImg} />
+				<h1 className={styles.name}>Ocean View</h1>
 				<div className={styles.overlay}></div>
 			</div>
 
 			<div className={styles.container}>
 
 				{
-					cardsTitles.map((title) => (
-						<HomeCard title={title}/>
+					cardsTitles.map((title, index) => (
+						<HomeCard key={index} title={title} index={index}/>
 					))
 				}
 
--- my-menu-project/src/components/HomeCard/HomeCard.jsx
+++ my-menu-project/src/components/HomeCard/HomeCard.jsx
@@ -2,11 +2,16 @@ import styles from './HomeCard.module.css'
 
 export default function HomeCard({
     title,
+    index
 }) {
     return (
         <>
             <div className={styles.card}>
-                <div className={styles.icon}><img src="kitchen.png" alt="kitchen image" /></div>
+                <div className={styles.icon}><img src={
+                    index == 0 ? 'kitchen.png' :
+                    index == 1 ? 'drinks.png' :
+                    index == 2 ? 'desert.png' : ''
+                } alt="kitchen image" /></div>
                 <div>
                     <div className={styles.divider}></div>
                     <div className={styles.title}>{title}</div>
