82aa04c2d04664f02a09ac80cf069589b490a3e5
LachezarBogdanov <lachezarbogdanov22@gmail.com>
2026-04-28 15:48:06 +0300

    Fixed the burger menu
    Fixed all of the burger manu styling. Changed the theme of the site

--- my-menu-project/src/components/BurgerMenu/BurgerMenu.jsx
+++ my-menu-project/src/components/BurgerMenu/BurgerMenu.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import styles from './BurgerMenu.module.css'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
+
 
 export default function BurgerMenu() {
 
@@ -8,15 +11,25 @@ export default function BurgerMenu() {
 
   return (
     <>
+
+        <div
+            className={`${styles.overlay} ${isActive ? styles.show : ''}`}
+            onClick={() => setIsActive(false)}
+        />
+
         <button className={styles.burger} id={styles.burger} onClick={() => setIsActive(prev => !prev)}>
             ☰
         </button>
 
-        <nav className={`${styles.menu} ${isActive ? styles.active : ''}`} id={styles.menu}>
-            <a href="#">Home</a>
-            <a href="#">About</a>
-            <a href="#">Contacts</a>
-        </nav>
+                <nav className={`${styles.menu} ${isActive ? styles.active : ''}`} id={styles.menu}>
+                    <div className={styles.wrapp}>
+                        <h2>Menu</h2>
+                        <div className={styles.close} onClick={() => setIsActive(false)}><FontAwesomeIcon icon={faXmark} /></div>
+                    </div>
+                    <a href="#">Кухня</a>
+                    <a href="#">Бар</a>
+                    <a href="#">Десерти</a>
+                </nav>
     </>
   );
 };
